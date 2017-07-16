e088b2dc41d55a98220708c3d72deec3a63051c9
Bailey Kursar <bkursar@gmail.com>
2017-07-17 00:14:09 +0100

    Having no existing records for responses in the database no longer breaks the program when run

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
     @messages = Message.all
+    if @messages.blank?
+      @array = @messages.order("RANDOM()")
+    else
+      @array = [{content: 'test'}, {content: 'test'}, {content: 'test'}, {content: 'test'}, {content: 'test'}, {content: 'test'}, {content: 'test'},]
+    end
   end
 end
