25e7bc0062e34a3a45b68c430d45cb94df9fce79
Bailey Kursar <bkursar@gmail.com>
2017-07-16 13:12:26 +0100

    Added some initial homepage content

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  root 'home#index'
+
   resources :responses
   resources :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
